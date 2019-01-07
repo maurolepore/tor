22f918a449a97dbe01fc61ef74523228169a1c84
maurolepore <maurolepore@gmail.com>
2019-01-07 07:41:39 -0500

    New load_tsv()

--- R/load_csv.R
+++ R/load_csv.R
@@ -1,4 +1,4 @@
-#' @rdname load_rds
+#' @rdname list_rds
 #' @export
 load_csv <- function(path = ".",
                      regexp = "[.]csv$",
@@ -35,4 +35,44 @@ load_csv <- function(path = ".",
   )
 
   list2env(lst, envir = envir)
+  invisible(path)
+}
+
+#' @rdname list_rds
+#' @export
+load_tsv <- function(path = ".",
+                     regexp = "[.]tsv$",
+                     ignore.case = TRUE,
+                     invert = FALSE,
+                     header = TRUE,
+                     sep = "\t",
+                     quote = "\"",
+                     dec = ".",
+                     fill = TRUE,
+                     comment.char = "",
+                     stringsAsFactors = FALSE,
+                     na.strings = c("", "NA"),
+                     envir = .GlobalEnv,
+                     ...) {
+  lst <- list_any(
+    path,
+    function(x) utils::read.csv(
+        file = x,
+        header = header,
+        sep = sep,
+        quote = quote,
+        dec = dec,
+        fill = fill,
+        comment.char = comment.char,
+        stringsAsFactors = stringsAsFactors,
+        na.strings = na.strings
+      ),
+    regexp = regexp,
+    ignore.case = ignore.case,
+    invert = invert,
+    ...
+  )
+
+  list2env(lst, envir = envir)
+  invisible(path)
 }
--- tests/testthat/test-load_csv.R
+++ tests/testthat/test-load_csv.R
@@ -1,9 +1,19 @@
 context("load_csv")
 
-test_that("load_csv loads multiple csv in a new environment", {
+test_that("load_csv loads multiple .csv files in a new environment", {
   e <- new.env()
   load_csv(tor_example("csv"), envir = e)
 
   expect_equal(c("file1", "file2"), ls(e))
+  rm(list = ls())
+})
+
+context("load_tsv")
+
+test_that("load_tsv loads multiple .tsv files in a new environment", {
+  e <- new.env()
+  load_tsv(tor_example("tsv"), envir = e)
+
+  expect_equal(c("tsv1", "tsv2"), ls(e))
   rm(list = ls())
 })
